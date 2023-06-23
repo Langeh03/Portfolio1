a5e3ee1bb7f2a9926950406b585464e791fdc03c
Langeh03 <langehmohammed7@gmail.com>
2023-06-23 15:00:57 +0100

    third commit

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require("dotenv").config;
 
 const MONGO_DB_CONFIG ={
-    DB: 'mongodb+srv://LangehMohammed:langeh03@backend.5kdaucn.mongodb.net/?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://LangehMohammed:langeh03@backend.5kdaucn.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
 }
 
 module.exports ={
